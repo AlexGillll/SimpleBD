cbc21a61ae5642c55cd16c62dfb1ed827b6652ec
AlexGillll <sashahylyuk@gmail.com>
2021-11-02 11:24:05 +0200

    Alex's commit

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ myBd.set('SashaV', '11/01/1991')
 
 print(myBd.get('SashaV'))
 
+myBd.resetBd()
+
+i = '0'
+while True:
+    i = str(input('key: '))
+    if i == '':
+        break
+    d = str(input('value: '))
+    myBd.set(i, d)
+
+
+
+
+
+
