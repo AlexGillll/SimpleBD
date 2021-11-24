e9e73d11a73bda4da5ad88708b2cf1bb29e873f4
Oleksandr <sashahylyuk@gmail.com>
2021-11-24 12:45:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from my_simple_bd import My_simple_bd
 
 myBd = My_simple_bd("./mybd.db")
-myBd.set('SashaG', '11/01/1990')
-myBd.set('SashaV', '11/01/1991')
+myBd.set('SashaG', '11/01/100')
+myBd.set('SashaV', '11/01/101')
 
 print(myBd.get('SashaV'))
 
